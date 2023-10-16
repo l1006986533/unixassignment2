1b0202250454d441a9693af627d16f490093d326
Liao <liao.yiqiang@qq.com>
2023-10-16 22:34:04 +0200

    select done

--- mathserver/src/server.c
+++ mathserver/src/server.c
@@ -7,12 +7,12 @@
 #include "matrix_inverse.h"
 
 int port=9999;
-int sd; //server file descriptor
+int server_socket;
 char originalWorkingDirectory[1024]="";
 
 int receive_ucn(int cd){
-    char string_ucn[255];
-    recv(cd, string_ucn, sizeof(string_ucn), 0);
+    char string_ucn[256];
+    recv(cd, string_ucn, 255, 0);
     int ucn=atoi(string_ucn);
     return ucn;
 }
@@ -35,7 +35,9 @@ int handle_command(int cd, char* command, int ucn, char* filename, char* filepat
         handling_kmeans_args(command, &k, input_file);
         //receive input file
         char temp[255];
-        sprintf(temp,"%s/../computed_results/client%d-%s",originalWorkingDirectory,ucn,input_file);
+        if(originalWorkingDirectory[0]=='\0')
+            sprintf(temp,"../computed_results/client%d-%s",ucn,input_file);
+        else sprintf(temp,"%s/../computed_results/client%d-%s",originalWorkingDirectory,ucn,input_file);
         strcpy(input_file, temp);
         recv_file(cd, input_file);
         run_kmeans(k, input_file, filepath); //get the parameters k and input_file, and write solution to filepath
@@ -74,13 +76,13 @@ void server_fork(){
     while (1) {
         struct sockaddr_in address;
         int addrlen = sizeof(address);
-        int cd = accept(sd, &address, &addrlen); // client file descriptor
+        int cd = accept(server_socket, &address, &addrlen); // client file descriptor
         if (cd < 0) {
             perror("accept failed");
             exit(EXIT_FAILURE);
         }
         if (fork() == 0) { // create child process
-            close(sd);
+            close(server_socket);
             handle_client_fork(cd);
             exit(0);
         }
@@ -89,9 +91,73 @@ void server_fork(){
     }
 }
 
+void server_select(){
+    int client_socket, max_sd, sd, activity, valread;
+    int max_clients = 30, client_socket_arr[30], matinv_sol_cnt[30], kmeans_sol_cnt[30];
+    char buffer[256];
+    fd_set readfds;
+    for (int i = 0; i < max_clients; i++) {
+        client_socket_arr[i] = 0;
+        kmeans_sol_cnt[i] = 1;
+        matinv_sol_cnt[i] = 1;
+    }
+
+    while(1) {
+        FD_ZERO(&readfds);
+        FD_SET(server_socket, &readfds);
+        max_sd = server_socket;
+        for (int i = 0; i < max_clients; i++) {
+            sd = client_socket_arr[i];
+            if(sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if(sd > max_sd) {
+                max_sd = sd;
+            }
+        }
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("select error");
+        }
+        if (FD_ISSET(server_socket, &readfds)) {
+            if ((client_socket = accept(server_socket, NULL, 0)) < 0) { 
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+            int ucn = receive_ucn(client_socket);
+            if(client_socket_arr[ucn] == 0)
+                client_socket_arr[ucn] = client_socket;
+            else printf("unique_client_number %d is used in this server",ucn);
+            printf("Connected with client %d\n",ucn);
+        }
+        for (int ucn = 0; ucn < max_clients; ucn++) {
+            sd = client_socket_arr[ucn];
+            if (FD_ISSET(sd, &readfds)) {
+                if ((valread = read(sd, buffer, 255)) == 0) {
+                    printf("Host %d disconnected\n", ucn);
+                    close(sd);
+                    client_socket_arr[ucn] = 0;
+                }
+                else {
+                    buffer[valread] = '\0';
+                    char filepath[255], filename[255];
+                    int result = handle_command(sd, buffer, ucn, filename, filepath, &matinv_sol_cnt[ucn], &kmeans_sol_cnt[ucn]);
+                    if(result == 0){
+                        printf("Sending solution: %s\n",filename);
+                        send(sd, filename, 255, 0);
+                        send_file(sd, filepath);
+                    }else if(result == 1){
+                        printf("Not start with matinvpar or kmeanspar\n");
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
     handling_server_args(argc,argv,&port,originalWorkingDirectory);
-    sd = bind_and_listen(port);
-    server_fork();
+    server_socket = bind_and_listen(port);
+    server_select();
 }
